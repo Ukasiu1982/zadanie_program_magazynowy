7e0e9de7569ccdb4cd07227b695edbb2f804f35c
Łukasz Ł <ukaszek1001@gmail.com>
2021-10-14 07:24:43 +0200

    dodano plik main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+import sys
+
+saldo = 0
+magazyn = {}
+historia = []
+
+while True:
+    komenda = []
+    akcja = input()
+    if akcja == 'stop':
+        break
+    elif akcja == "saldo":
+        komenda.append('saldo')
+        komenda.append(input())
+        komenda.append(input())
+        historia.append(komenda)
+    elif akcja == "zakup":
+        komenda.append('zakup')
+        komenda.append(input())
+        komenda.append(input())
+        komenda.append(input())
+        historia.append(komenda)
+    elif akcja == "sprzedaz":
+        komenda.append('sprzedaz')
+        komenda.append(input())
+        komenda.append(input())
+        komenda.append(input())
+        historia.append(komenda)
+    else:
+        print('Niedozwolona akcja')
+        break
+
+komenda = sys.argv[1:]
+historia.append(komenda)
+
+for komenda in historia:
+    akcja = komenda[0]
+    if akcja == 'saldo':
+        saldo += int(komenda[1])
+    elif akcja =='zakup':
+        produkt = komenda[1]
+        cena = int(komenda[2])
+        ilosc = int(komenda[3])
+    if saldo >= cena*ilosc:
+        saldo -= ilosc * cena
+    if magazyn.get(produkt):
+        magazyn[produkt] += ilosc
+    else:
+        magazyn[produkt] = ilosc
+        else:
+        print('nie mozna kupic')
+        break
+    elif akcja == 'sprzedaz':
+        produkt = komenda[1]
+        cena = int(komenda[2])
+        ilosc = int(komenda[3])
+    if magazyn.get(produkt) and magazyn.get(produkt) >= ilosc:
+        saldo += ilosc * cena
+        magazyn[produkt] -= ilosc
+        else:
+        print('za malo produktow do transkacji')
+        break
+    elif akcja == 'konto':
+        print('stan konta:', saldo)
+    elif akcja == 'magazyn':
+        print('stan magazynu:', magazyn)
+    elif akcja == 'przeglad':
+        od = int(komenda[2])
+        do = int(komenda[3])
+        print(historia[od:do])
